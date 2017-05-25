d05ad26f9f36e83be02f9ed43c5770353707dfdb
Alexander Midjich <Alexander Midjich>
2017-05-25 10:25:58 +0200

    updated code for renting a car

--- modules/hyr.js
+++ modules/hyr.js
@@ -6,7 +6,23 @@ module.exports = function(app){
 });
 
 app.post('/hyr', (req, res) => {
-console.log(req.body)
-});
+  console.log(req.body)
+  var search = {};
+  if(req.body.seats !== '')
+    search.seats = req.body.seats;
+  if(req.body.gear !== '')
+    search.gear = req.body.gear;
+  if(req.body.rails !== '')
+    search.rails = req.body.rails;
+  if(req.body.tow !== '')
+    search.tow = req.body.tow;
 
+  console.log(search);
+
+  mongo.db.collection('cars').find(search).toArray((error, result) => {
+    for(i=0; i<result.length; i++)
+      console.log(result[i]);
+      res.render('hyr', {'results':result});
+  });
+});
 }
